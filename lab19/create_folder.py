b82f125154d83effa7737d49816e94bae4407dc0
Anthony Lopez <Antl21292@gmail.com>
2023-05-20 19:40:12 -0400

    added exception handling

--- lab19/create_folder.py
+++ lab19/create_folder.py
@@ -5,5 +5,10 @@ console = Console()
 
 
 def create_folder(folder_name):
-    os.mkdir(folder_name)
-    console.print(f"[green]Directory {folder_name} created successfully![/green]")
+    try:
+        os.mkdir(folder_name)
+        # console.print(f"[green]Directory {folder_name} created successfully![/green]")
+    except FileExistsError:
+        console.print(f"[red]Directory {folder_name} already exists![/red]")
+
+# create_folder("./deleted/test1")
