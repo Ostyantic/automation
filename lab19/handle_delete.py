2d61758626a0ab45ed4b97a6cce57450f0a67c8a
Anthony Lopez <Antl21292@gmail.com>
2023-05-20 21:37:26 -0400

    small change

--- lab19/handle_delete.py
+++ lab19/handle_delete.py
@@ -3,15 +3,17 @@ import shutil
 from create_folder import create_folder
 
 
-def handle_delete(user):
+def handle_delete(file_path):
     dst_folder = "temporary"
 
     if not os.path.exists(dst_folder):
         create_folder(dst_folder)
 
-    for file_name in os.listdir(f"./users/{user}"):
-        file_path = os.path.join(f"./users/{user}", file_name)
-        shutil.move(file_path, dst_folder)
+    for file_name in os.listdir(file_path):
+        new_file_path = os.path.join(file_path, file_name)
+        shutil.move(new_file_path, dst_folder)
 
 
-# handle_delete("user1")
+handle_delete("./users/user1")
+handle_delete("./users/user2")
+
