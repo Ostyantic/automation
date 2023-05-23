bba48bc3870f0a44374cbf80e6a2350b612cdaf2
Anthony Lopez <Antl21292@gmail.com>
2023-05-23 00:55:58 -0400

    finished

--- lab19/create_folder.py
+++ lab19/create_folder.py
@@ -5,10 +5,10 @@ console = Console()
 
 
 def create_folder(folder_name):
-    try:
-        os.mkdir(folder_name)
-        # console.print(f"[green]Directory {folder_name} created successfully![/green]")
-    except FileExistsError:
-        console.print(f"[red]Directory {folder_name} already exists![/red]")
+    # try:
+    os.mkdir(folder_name)
+    # console.print(f"[green]Directory {folder_name} created successfully![/green]")
+    # except FileExistsError:
+    #     console.print(f"[red]Directory {folder_name} already exists![/red]")
 
 # create_folder("./deleted/test1")
--- lab19/main.py
+++ lab19/main.py
@@ -22,31 +22,38 @@ def main():
     list_directory()
 
     while True:
-        user_selection = Prompt.ask("\nPlease select an option")
-        if not user_selection.isnumeric() or int(user_selection) > len(directory):
+        user_selection = Prompt.ask("\nPlease select an option. 'q' to quit.")
+
+        if user_selection == "q":
+            break
+        elif not user_selection.isnumeric() or int(user_selection) > len(directory):
             console.print("\n[red]Invalid Selection![/red]")
         elif user_selection == "1":
-            file_path = Prompt.ask("Please enter the file path")
-            directory_name = Prompt.ask("Please enter the name of the directory")
-            directory_path = os.path.join(file_path, directory_name)
-            create_folder(directory_path)
-            break
+            try:
+                file_path = Prompt.ask("Please enter the file path")
+                directory_name = Prompt.ask("Please enter the name of the directory")
+                directory_path = os.path.join(file_path, directory_name)
+                create_folder(directory_path)
+            except FileExistsError:
+                console.print(f"[red]Directory {directory_name} already exists![/red]")
+            console.print(f"[green]Directory {directory_name} created successfully!")
         elif user_selection == "2":
-            pass
+            file_path = Prompt.ask("Please enter the file path")
+            directory_name = Prompt.ask("Please enter the name of the directory you would like to delete")
+            directory_path = os.path.join(file_path, directory_name)
+            handle_delete(directory_path)
         elif user_selection == "3":
-            pass
+            file_path = Prompt.ask("Please enter the file path")
+            handle_sort(file_path)
         elif user_selection == "4":
-            pass
+            file_path = Prompt.ask("Please enter the file path")
+            file_name = Prompt.ask("Please enter the name of the file you would like to parse")
+            directory_path = os.path.join(file_path, file_name)
+            parse(directory_path)
         elif user_selection == "5":
-            pass
-
-        else:
-            break
-
-
-
-
-    console.print(f"You have selected {directory[int(user_selection) - 1]}")
+            file_path = Prompt.ask("Please enter a file path")
+            pattern = Prompt.ask("Please enter the file extension")
+            list_files(file_path, pattern)
 
 
 if __name__ == "__main__":
